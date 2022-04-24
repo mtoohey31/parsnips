d73de8af5a883d6fe12fec68b986082d485c4e39
Nathan Gerryts <nathgerr@gmail.com>
2022-04-24 19:04:09 -0400

    repr storage testing

--- parsnips-emu/src/lib.rs
+++ parsnips-emu/src/lib.rs
@@ -1,8 +1,42 @@
+use ux::*;
+
+#[repr(packed)]
+pub struct Inst(u6);
+
+const R_INST: Inst = Inst(ux::u6::new(0b00001000u8));
+
+
+pub struct Instruction {
+    opcode: u5,
+    data: u27,
+}
+
+pub struct Emulator {
+    registers: [u32; 32],
+    // cmp_flags: ???,
+    program: Vec<Instruction>,
+}
+
+
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    use ux::*;
+    use super::*;
+    use std::mem::size_of;
+
+    #[repr(packed)]
+    struct oool{
+        type1: u2, 
+        type2: u6
+    
     }
+    
+
+    #[test]
+    fn it_takes_32_bytes() {
+        assert_eq!(size_of::<oool>(), 1)
+    }
+
+    
 }
