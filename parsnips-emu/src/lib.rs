df3624ffc3c30aec3321a3b81d938cbb09abc202
Matthew Toohey <contact@mtoohey.com>
2022-04-26 16:33:39 -0400

    fix: handle overflows and signs correctly in ADD* operations

--- parsnips-emu/src/lib.rs
+++ parsnips-emu/src/lib.rs
@@ -2,6 +2,18 @@
 
 mod inst;
 use inst::{opcode::*, Inst};
+use std::{error::Error, fmt};
+
+#[derive(Debug)]
+pub enum EmulatorError {
+    Overflow,
+}
+impl fmt::Display for EmulatorError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "overflow occurred")
+    }
+}
+impl Error for EmulatorError {}
 
 pub struct Emulator {
     registers: [u32; 32],
@@ -21,7 +33,7 @@ impl Emulator {
         };
     }
 
-    pub fn step(&mut self) {
+    pub fn step(&mut self) -> Result<(), EmulatorError> {
         use inst::InstFields;
 
         let inst = &self.program[self.program_counter];
@@ -32,7 +44,19 @@ impl Emulator {
                 use inst::RegFields;
 
                 match inst.funct() {
+                    // NOTE: the difference between ADD and ADDU is that ADD
+                    // generates a trap when an overflow occurs
                     ADD => {
+                        use inst::ArithLogFields;
+
+                        match (self.registers[inst.rs()] as i32)
+                            .checked_add(self.registers[inst.rt()] as i32)
+                        {
+                            Some(s) => self.registers[inst.rd()] = s as u32,
+                            None => return Err(EmulatorError::Overflow),
+                        }
+                    }
+                    ADDU => {
                         use inst::ArithLogFields;
 
                         self.registers[inst.rd()] = unsafe {
@@ -45,17 +69,32 @@ impl Emulator {
             ADDI => {
                 use inst::ArithLogIFields;
 
+                // NOTE: casts back and forth ensure sign extension
+                match (self.registers[inst.rs()] as i32).checked_add(inst.imm() as i16 as i32) {
+                    Some(s) => self.registers[inst.rt()] = s as u32,
+                    None => return Err(EmulatorError::Overflow),
+                }
+            }
+            ADDIU => {
+                use inst::ArithLogIFields;
+
+                // NOTE: lack of casts back and forth ensure zero extension
                 self.registers[inst.rt()] =
-                    self.registers[inst.rs()] + inst.imm() as i16 as i32 as u32;
+                    unsafe { self.registers[inst.rs()].unchecked_add(inst.imm() as u32) };
             }
             _ => todo!(),
         }
+
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::error::Error;
+
+    type RUE = Result<(), Box<dyn Error>>;
 
     macro_rules! step_with {
         ( $( $x:expr ),* ) => {
@@ -65,42 +104,48 @@ mod tests {
                 )*]);
                 $(
                     $x;
-                    emu.step();
+                    emu.step()?;
                 )*
                 emu
             }
         };
     }
 
+    // TODO: add tests to check for overflows
+
     #[test]
-    fn addi_pos() {
+    fn addi_pos() -> RUE {
         let emu = step_with![0b001000_00000_00001_0111111111111111];
         assert_eq!(emu.registers[1], i16::max_value() as u32);
+        Ok(())
     }
 
     #[test]
-    fn addi_neg() {
+    fn addi_neg() -> RUE {
         let emu = step_with![0b001000_00000_00001_1111111111111111];
         assert_eq!(emu.registers[1], -1 as i16 as i32 as u32);
+        Ok(())
     }
 
     #[test]
-    fn add_pos() {
+    fn add_pos() -> RUE {
         let emu = step_with![
             0b001000_00000_00011_0000000000000010,
             0b001000_00000_00010_0000000000000001,
             0b000000_00010_00011_00100_00101_100000
         ];
         assert_eq!(emu.registers[4], 3);
+        Ok(())
     }
 
     #[test]
-    fn add_neg() {
+    fn add_neg() -> RUE {
         let emu = step_with![
             0b001000_00000_00011_1111111111111111,
             0b001000_00000_00010_1111111111111111,
             0b000000_00010_00011_00100_00101_100000
         ];
         assert_eq!(emu.registers[4], -2 as i32 as u32);
+        Ok(())
     }
 }
