0802fd70191711751b2bccbb5f2096384ec692c3
Matthew Toohey <contact@mtoohey.com>
2022-08-06 00:47:02 -0400

    refactor!: convert opcode and function values to deriving enums

--- parsnips-inst/src/function.rs
+++ parsnips-inst/src/function.rs
@@ -1,26 +1,35 @@
-pub const SLL: u8 = 0b000000;
-pub const SRL: u8 = 0b000010;
-pub const SRA: u8 = 0b000011;
-pub const SLLV: u8 = 0b000100;
-pub const SRLV: u8 = 0b000110;
-pub const SRAV: u8 = 0b000111;
-pub const JR: u8 = 0b001000;
-pub const JALR: u8 = 0b001001;
-pub const MFHI: u8 = 0b010000;
-pub const MTHI: u8 = 0b010001;
-pub const MFLO: u8 = 0b010010;
-pub const MTLO: u8 = 0b010011;
-pub const MULT: u8 = 0b011000;
-pub const MULTU: u8 = 0b011001;
-pub const DIV: u8 = 0b011010;
-pub const DIVU: u8 = 0b011011;
-pub const ADD: u8 = 0b100000;
-pub const ADDU: u8 = 0b100001;
-pub const SUB: u8 = 0b100010;
-pub const SUBU: u8 = 0b100011;
-pub const AND: u8 = 0b100100;
-pub const OR: u8 = 0b100101;
-pub const XOR: u8 = 0b100110;
-pub const NOR: u8 = 0b100111;
-pub const SLT: u8 = 0b101010;
-pub const SLTU: u8 = 0b101001;
+use num_enum::{IntoPrimitive, TryFromPrimitive};
+use strum_macros::EnumString;
+
+#[cfg_attr(test, derive(Debug))]
+#[derive(Eq, PartialEq, TryFromPrimitive, IntoPrimitive, EnumString)]
+#[strum(serialize_all = "lowercase")]
+#[repr(u8)]
+pub enum Funct {
+    SLL = 0b000000,
+    SRL = 0b000010,
+    SRA = 0b000011,
+    SLLV = 0b000100,
+    SRLV = 0b000110,
+    SRAV = 0b000111,
+    JR = 0b001000,
+    JALR = 0b001001,
+    MFHI = 0b010000,
+    MTHI = 0b010001,
+    MFLO = 0b010010,
+    MTLO = 0b010011,
+    MULT = 0b011000,
+    MULTU = 0b011001,
+    DIV = 0b011010,
+    DIVU = 0b011011,
+    ADD = 0b100000,
+    ADDU = 0b100001,
+    SUB = 0b100010,
+    SUBU = 0b100011,
+    AND = 0b100100,
+    OR = 0b100101,
+    XOR = 0b100110,
+    NOR = 0b100111,
+    SLT = 0b101010,
+    SLTU = 0b101001,
+}
